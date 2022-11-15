d04bcc6e961efb0c13b3c79ccac8f167fd5f75ae
Peter Dunn <pdunn2@usc.edu.au>
2022-11-15 12:41:00 +1000

    Updates

--- _common.R
+++ _common.R
@@ -8,14 +8,16 @@ knitr::opts_chunk$set(
   )
 
 
-
+# Translucent colour:
 plotColour <- rgb(red = 47/256, 
                   green = 105/256, 
                   blue = 113/256, 
                   alpha = 0.6, 
                   maxColorValue = 1)
-plotColour1 <- rgb(red = 47/256, 
-                   green = 105/256, 
-                   blue = 113/256, 
+
+# Solid colour:
+plotColour1 <- rgb(red = 129/256, 
+                   green = 194/256, 
+                   blue = 202/256, 
                    alpha = 1, 
                    maxColorValue = 1)
