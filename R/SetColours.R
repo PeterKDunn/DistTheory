c31b7e75de5303cc3fb342c335456b205b9c41af
Peter Dunn <pdunn2@usc.edu.au>
2026-06-17 17:20:39 +1000

    Add Very Light colour; change :Light colour

--- R/SetColours.R
+++ R/SetColours.R
@@ -4,12 +4,16 @@
 if ( knitr::is_latex_output() ) {
   ColourSolid  <- "black"
   ColourLight  <- "darkgrey"
+  ColourVeryLight  <- "lightgrey"
 } else {
   ColourSolid <- "cyan4" # 0, 139, 139
-  ColourLight <- "darkslategray3"
+  ColourLight <- "lightcyan3"
+  ColourVeryLight <- "lightcyan"
 }
 ColourOpaque <- adjustcolor(ColourSolid, 
                             alpha.f = 0.6)
 ColourLightOpaque <- adjustcolor(ColourLight, 
                             alpha.f = 0.6)
+ColourVeryLightOpaque <- adjustcolor(ColourVeryLight, 
+                                 alpha.f = 0.6)
 
